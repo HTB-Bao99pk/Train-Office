e1f39a72de67dca8b1a44739a28727eeda5eaa23
Huynh Bao <huynhthienbao294294@gmail.com>
2026-05-29 14:18:51 +0700

    Cập nhật pom, appli, user entity

---
+++ src/main/java/com/hsf302/trainoffice/common/Role.java
@@ -0,0 +1,6 @@
+package com.hsf302.trainoffice.common;
+
+public enum Role {
+    STAFF,
+    CUSTOMER
+}
--- src/main/java/com/hsf302/trainoffice/entity/User.java
+++ src/main/java/com/hsf302/trainoffice/entity/User.java
@@ -1,4 +1,44 @@
 package com.hsf302.trainoffice.entity;
 
-public class User {
+
+import com.hsf302.trainoffice.common.Role;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Getter
+@Setter
+@Table(name = "users")
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "email", nullable = false, columnDefinition = "NVARCHAR(255)")
+    private String email;
+
+    @Column(name = "password", nullable = false, columnDefinition = "NVARCHAR(255)")
+    private String password;
+
+    @Column(name = "full_name", nullable = false, columnDefinition = "NVARCHAR(100)")
+    private String fullName;
+
+    @Column(name = "phone", nullable = false, columnDefinition = "NVARCHAR(20)")
+    private String phone;
+
+    @Column(name = "create_date")
+    private LocalDate createDate;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false)
+    private Role role;
+
+
 }
