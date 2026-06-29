4fe5816ef04032c7bb0175980909bee67bb6822b
LamAnhNhu <se193810nguyenthianhnhu@gmail.com>
2026-06-29 20:24:37 +0700

    code da hoan thien, forgot password hoan thien

--- src/main/java/com/hsf302/trainoffice/service/impl/UserServiceImpl.java
+++ src/main/java/com/hsf302/trainoffice/service/impl/UserServiceImpl.java
@@ -32,6 +32,11 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    @Override
     public boolean register(RegisterRequest registerRequest) {
         String fullName = registerRequest.getFullName();
         String email = registerRequest.getEmail();
